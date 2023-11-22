ff4a95cb381750ebe0fb02a56b2488cbdf5ce938
TheXDShrimp <xdshrimphead@gmail.com>
2023-11-22 16:49:35 -0500

    backend fixes

--- app/src/components/Canvas.tsx
+++ app/src/components/Canvas.tsx
@@ -1,58 +1,62 @@
 import React, { useRef, useEffect } from 'react'
 
 const Canvas = (props) => {
-  const { draw, ...rest } = props
-  const canvasRef = useRef(null)
+  // const { draw, ...rest } = props
+  // const canvasRef = useRef(null)
 
-  const resizeCanvas = (canvas) => {
-    const { width, height } = canvas.getBoundingClientRect()
+  // const resizeCanvas = (canvas) => {
+  //   const { width, height } = canvas.getBoundingClientRect()
     
-    if (canvas.width !== width || canvas.height !== height) {
-      const { devicePixelRatio:ratio=1 } = window
-      const context = canvas.getContext('2d')
-      canvas.width = width*ratio
-      canvas.height = height*ratio
-      context.scale(ratio, ratio)
-      return true
-    }
+  //   if (canvas.width !== width || canvas.height !== height) {
+  //     const { devicePixelRatio:ratio=1 } = window
+  //     const context = canvas.getContext('2d')
+  //     canvas.width = width*ratio
+  //     canvas.height = height*ratio
+  //     context.scale(ratio, ratio)
+  //     return true
+  //   }
 
-    return false
-  }
+  //   return false
+  // }
   
-  const predraw = (context, canvas) => {
-    context.save()
-    resizeCanvas(canvas)
-    const { width, height } = context.canvas
-    context.clearRect(0, 0, width, height)
-  }
+  // const predraw = (context, canvas) => {
+  //   context.save()
+  //   resizeCanvas(canvas)
+  //   const { width, height } = context.canvas
+  //   context.clearRect(0, 0, width, height)
+  // }
 
-  const postdraw = (context, canvas) => {
+  // const postdraw = (context, canvas) => {
     
-  }
+  // }
 
-  useEffect(() => {
+  // useEffect(() => {
     
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
-    let frameCount = 0
-    let animationFrameId
+  //   const canvas = canvasRef.current
+  //   const context = canvas.getContext('2d')
+  //   let frameCount = 0
+  //   let animationFrameId
     
-    const render = () => {
-      frameCount++
-      draw(context, frameCount)
-      animationFrameId = window.requestAnimationFrame(render)
-    }
+  //   const render = () => {
+  //     frameCount++
+  //     draw(context, frameCount)
+  //     animationFrameId = window.requestAnimationFrame(render)
+  //   }
     
-    predraw(context, canvas)
-    render()
-    postdraw(context, canvas)
+  //   predraw(context, canvas)
+  //   render()
+  //   postdraw(context, canvas)
 
-    return () => {
-      window.cancelAnimationFrame(animationFrameId)
-    }
-  }, [draw])
+  //   return () => {
+  //     window.cancelAnimationFrame(animationFrameId)
+  //   }
+  // }, [draw])
   
-  return <canvas ref={canvasRef} {...rest}/>
+  // return <canvas ref={canvasRef} {...rest}/>
+
+  return (
+    <p>Wahoo</p>
+  )
 }
 
 export default Canvas
--- app/src/components/footer.tsx
+++ app/src/components/footer.tsx
@@ -6,11 +6,11 @@ import SiteLink from './SiteLink';
 export const Footer = ({bgColor='bg-navy-darkest'}) => {
     return (
         <footer className={`relative w-full ${bgColor} pb-4 bg-opacity-100`}>
-            <p className='mt-4 text-white text-center text-base'>Made with 💖 by <SiteLink href={"https://arulandu.com"} txt="Alvan Caleb Arulandu"/> {' '}
+            {/* <p className='mt-4 text-white text-center text-base'>Made with 💖 by <SiteLink href={"https://arulandu.com"} txt="Alvan Caleb Arulandu"/> {' '}
             <a className='inline-block align-middle' href="https://github.com/Claeb101/spring-institute">
                 <img className='h-full' src="https://img.shields.io/github/last-commit/claeb101/spring-institute"/>
             </a>
-            </p>
+            </p> */}
         </footer>
     );
 }
