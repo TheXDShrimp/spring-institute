173ae23462ef073d7f1f4e75a683c2b6d8339dac
Brian Zhou <bzhou2024@gmail.com>
2023-11-30 15:43:38 -0500

    Update peopleData.ts

--- app/src/pages/api/peopleData.ts
+++ app/src/pages/api/peopleData.ts
@@ -85,6 +85,177 @@ const peopleData = [
 
 
     {
+        stateName: "Maryland",
+        nameDisplay: "DC-Maryland-Virginia Region",
+        contact: "chapter.dmv@thespringgroup.org",
+        overseer: "Brian Zhou",
+        region: "Midatlantic",
+        regionalDirectors: [
+            {
+                name: "Aileen Wu",
+                title: "Director, Virginia Region",
+                image: "awu.png",
+                description: "Aileen is a sophomore at McLean High School in McLean, Virginia. She is interested in computer science, policy, economics, and the medical sciences. Outside of SPRING, Aileen is the captain of McLean's Public Forum debate team and a researcher. She enjoys playing volleyball, piano, reading, and traveling."
+            },
+
+            {
+                name: "Tobin Wilson",
+                title: "Director, Virginia Region",
+                image: "twilson.png",
+                description: "A freshmen at McLean High School, Tobin is interested in exploring the intersections of public policy, philosophy, and jurisprudence. Alongside SPRING, Tobin coaches debate at his local middle school. He enjoys reading, competitive mountain biking, and playing the violin."
+            },
+
+            {
+                name: "Arman Syed",
+                title: "Director, DC-Maryland Region",
+                image: "asyed.png",
+                description: "A member of the class of 2025 at Sidwell Friends School in Washington DC, Arman is interested in both economics and international relations. Additionally to SPRING, Arman is an avid debater as well as also enjoying things like playing chess and spending time with friends."
+            },
+
+            {
+                name: "Frederick Bao",
+                title: "Director, DC-Maryland Region",
+                image: "fbao.png",
+                description: "Fred is a sophomore at Sidwell Friends High School in Washington D.C. Having been interested in politics from a very young age, Fred closely follows current events, local and national politics. Outside of SPRING, Fred is a National Go Champion and does debate. He enjoys playing ice hockey, watching football, and collecting cards."
+            },
+
+        ],
+        fellows: [
+            {
+                name: "Abhiram Chavali",
+                position: "Fellow from Virginia",
+                image: "achavali.jfif",
+                description: "A freshman at Thomas Jefferson High School for Science and Technology in Alexandria, Virginia, Abhiram is interested in studying law, international relations, and politics. Outside of SPRING, Abhiram enjoys competing in Model UN, reading, and spending time with friends."
+            },
+
+            {
+                name: "Akshat Alok",
+                position: "Fellow from Virginia",
+                image: "aalok.png",
+                description: "A junior at Thomas Jefferson High School for Science and Technology in Alexandria, Virginia, Akshat is interested in studying the intersection of Computer Science, in particular machine learning, and Biology. Outside of SPRING, Akshat is a researcher, and enjoys hobbies like basketball, football, and listening to music."
+            },
+
+            {
+                name: "Camille Stephant",
+                position: "Fellow from Virginia",
+                image: "cstephant.png",
+                description: "A junior at McLean High in northern Virginia, Camille is interested in studying science, technology, and international affairs, focusing on biological concepts and the ethical application of new technologies throughout the world. Camille is the captain of her track team and the vice president of her high school’s UNICEF club. She enjoys hiking, baking, running, taking pictures, and spending time with friends."
+            },
+
+            {
+                name: "George Schunk",
+                position: "Fellow from Virginia",
+                image: "gschunk.png",
+                description: "A junior at Washington-Liberty High School in Arlington, Virginia, George is interested in studying environmental foreign policy. Outside of SPRING, George is a varsity rower and a climber. He enjoys skateboarding, spending time with friends, and painting."
+            },
+
+            {
+                name: "Ronit Kapur",
+                position: "Fellow from Virginia",
+                image: "rkapur.png",
+                description: "A current junior at Thomas Jefferson High School for Science and Technology in Alexandria, Virginia, Ronit is keen to explore both computational astrophysics and science policy. Outside of SPRING, Ronit conducts research and holds leadership positions in his school's Science Olympiad team, astronomy club, and astronomy team. He enjoys traveling, playing the piano, and photographing the night sky."
+            },
+
+            {
+                name: "Shaun Jones",
+                position: "Fellow from Virginia",
+                image: "sjones.png",
+                description: "A junior at Basis Independent McLean in McLean, Virginia, Shaun is interested in both social and economic policy on a nationwide scale. Outside of SPRING, Shaun is the founder of his school's political science club and is a core member of his school's debate team. He enjoys playing soccer, videogames, and video editing."
+            },
+        ]
+    },
+
+
+
+
+
+
+    {
+        stateName: "DC",
+        nameDisplay: "DC-Maryland-Virginia Region",
+        contact: "chapter.dmv@thespringgroup.org",
+        overseer: "Brian Zhou",
+        region: "Midatlantic",
+        regionalDirectors: [
+            {
+                name: "Aileen Wu",
+                title: "Director, Virginia Region",
+                image: "awu.png",
+                description: "Aileen is a sophomore at McLean High School in McLean, Virginia. She is interested in computer science, policy, economics, and the medical sciences. Outside of SPRING, Aileen is the captain of McLean's Public Forum debate team and a researcher. She enjoys playing volleyball, piano, reading, and traveling."
+            },
+
+            {
+                name: "Tobin Wilson",
+                title: "Director, Virginia Region",
+                image: "twilson.png",
+                description: "A freshmen at McLean High School, Tobin is interested in exploring the intersections of public policy, philosophy, and jurisprudence. Alongside SPRING, Tobin coaches debate at his local middle school. He enjoys reading, competitive mountain biking, and playing the violin."
+            },
+
+            {
+                name: "Arman Syed",
+                title: "Director, DC-Maryland Region",
+                image: "asyed.png",
+                description: "A member of the class of 2025 at Sidwell Friends School in Washington DC, Arman is interested in both economics and international relations. Additionally to SPRING, Arman is an avid debater as well as also enjoying things like playing chess and spending time with friends."
+            },
+
+            {
+                name: "Frederick Bao",
+                title: "Director, DC-Maryland Region",
+                image: "fbao.png",
+                description: "Fred is a sophomore at Sidwell Friends High School in Washington D.C. Having been interested in politics from a very young age, Fred closely follows current events, local and national politics. Outside of SPRING, Fred is a National Go Champion and does debate. He enjoys playing ice hockey, watching football, and collecting cards."
+            },
+
+        ],
+        fellows: [
+            {
+                name: "Abhiram Chavali",
+                position: "Fellow from Virginia",
+                image: "achavali.jfif",
+                description: "A freshman at Thomas Jefferson High School for Science and Technology in Alexandria, Virginia, Abhiram is interested in studying law, international relations, and politics. Outside of SPRING, Abhiram enjoys competing in Model UN, reading, and spending time with friends."
+            },
+
+            {
+                name: "Akshat Alok",
+                position: "Fellow from Virginia",
+                image: "aalok.png",
+                description: "A junior at Thomas Jefferson High School for Science and Technology in Alexandria, Virginia, Akshat is interested in studying the intersection of Computer Science, in particular machine learning, and Biology. Outside of SPRING, Akshat is a researcher, and enjoys hobbies like basketball, football, and listening to music."
+            },
+
+            {
+                name: "Camille Stephant",
+                position: "Fellow from Virginia",
+                image: "cstephant.png",
+                description: "A junior at McLean High in northern Virginia, Camille is interested in studying science, technology, and international affairs, focusing on biological concepts and the ethical application of new technologies throughout the world. Camille is the captain of her track team and the vice president of her high school’s UNICEF club. She enjoys hiking, baking, running, taking pictures, and spending time with friends."
+            },
+
+            {
+                name: "George Schunk",
+                position: "Fellow from Virginia",
+                image: "gschunk.png",
+                description: "A junior at Washington-Liberty High School in Arlington, Virginia, George is interested in studying environmental foreign policy. Outside of SPRING, George is a varsity rower and a climber. He enjoys skateboarding, spending time with friends, and painting."
+            },
+
+            {
+                name: "Ronit Kapur",
+                position: "Fellow from Virginia",
+                image: "rkapur.png",
+                description: "A current junior at Thomas Jefferson High School for Science and Technology in Alexandria, Virginia, Ronit is keen to explore both computational astrophysics and science policy. Outside of SPRING, Ronit conducts research and holds leadership positions in his school's Science Olympiad team, astronomy club, and astronomy team. He enjoys traveling, playing the piano, and photographing the night sky."
+            },
+
+            {
+                name: "Shaun Jones",
+                position: "Fellow from Virginia",
+                image: "sjones.png",
+                description: "A junior at Basis Independent McLean in McLean, Virginia, Shaun is interested in both social and economic policy on a nationwide scale. Outside of SPRING, Shaun is the founder of his school's political science club and is a core member of his school's debate team. He enjoys playing soccer, videogames, and video editing."
+            },
+        ]
+    },
+
+
+
+
+
+    {
         stateName: "New Jersey",
         nameDisplay: "New Jersey, Pennsylvania, New York Tri-State Area",
         contact: "chapter.panjny@thespringgroup.org",
