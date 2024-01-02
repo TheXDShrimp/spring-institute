c621cfcd32605e11b890066da230ba69bfedab71
Brian <bzhou2024@gmail.com>
2024-01-01 19:16:25 -0500

    Update nationalDirectorsData.ts

--- app/src/pages/api/nationalDirectorsData.ts
+++ app/src/pages/api/nationalDirectorsData.ts
@@ -1,7 +1,7 @@
 export const nationalDirectorsData = [
     {
         name: "Brian Zhou",
-        position: "Founder & Chairman",
+        position: "Founder & Executive Director",
         image: "bzhou.png",
         description: "A senior at Thomas Jefferson High School for Science and Technology in Alexandria, Virginia, Brian is interested in studying the intersections between policy, economics, and the computational sciences. Outside of SPRING, Brian is a captain of his school's debate team and a researcher. He enjoys hiking, cooking, and exploring new places."
     },
